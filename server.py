1134c140d1d15250600621a0a77df284e5b7aba4
Kellie Grieneisen <kellie_grieneisen@aol.com>
2021-08-02 21:06:54 +0000

    added requirements, env, and server.

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import (Flask, render_template, request, flash, session,
+                   redirect)
+
+app = Flask(__name__)
+# app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def homepage():
+    """Homepage displaying contact info, links and projects."""
+    
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', debug=True)
